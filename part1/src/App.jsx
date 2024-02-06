f6216c162c6c1ab398f5f0334d218f293cd57184
21797545 <u21797545@tuks.co.za>
2024-02-06 12:16:14 +0200

    Update App.jsx to pass an array as props
    Updated App.jsx such that it conforms to instructions and passes an array as a prop to the Content and Total Components.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,42 +1,60 @@
 const Header = (props) => {
+    console.log(props);
     return(
       <h1>{props.course}</h1>
     )
 }
 
 const Part = (props) => {
+  console.log(props);
   return(
     <p>{props.name} {props.number}</p>
   )
 }
 
 const Content = (props) => {
-  
+ console.log(props)
   return(
     <div>
-      <Part name={props.part1} number={props.exercises1}/>
-      <Part name={props.part2} number={props.exercises2}/>
-      <Part name={props.part3} number={props.exercises3}/>
+      <Part name={props.parts[0].name} number={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} number={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} number={props.parts[2].exercises}/>
     </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return(
+    <p>Number of exercises: {props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises}</p>
   )
 }
 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts =  [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  },
+]
 
   return(
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1}
-      part2={part2} exercises2={exercises2} part3={part3}
-      exercises3={exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts}/>
     </div>
   )
   /*return (
